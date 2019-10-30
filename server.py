f18373cbce5d63d7094880a0f4ccb11075db0a2d
Meleny <865622793@qq.com>
2019-10-30 15:56:02 +0800

    implement return face_locations

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import request, Flask
 import time
 import os
 import base64
+import face
 
 app = Flask(__name__)
 
@@ -27,9 +28,13 @@ def get_frame():
         file = open('result.png', 'wb')
         file.write(image_data)
         file.close()
-        return 'success'
+        face_location = face.locate('result.png')
+        return {"locations": face_location}
     else:
         return 'fail'
+
+# todo:return so little thing. Need to return names too.
+# todo:deal with the client
 
 
 if __name__ == "__main__":
